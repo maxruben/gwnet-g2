4e47a77145f57da78fbb6921c4573039275f9ad6
rub <rub@svep.se>
2023-02-24 14:56:12 +0100

    Initialized to run the CAF sample

--- app/src/main.c
+++ app/src/main.c
@@ -1,47 +1,21 @@
 /*
  * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
+#include <app_event_manager.h>
 
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-
-#include "app_version.h"
+#define MODULE main
+#include <caf/events/module_state_event.h>
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(MODULE);
 
-void main(void)
-{
-	int ret;
-	const struct device *sensor;
-
-	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return;
-	}
-
-	while (1) {
-		struct sensor_value val;
-
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
-	}
+void main(void) {
+  LOG_INF("Starting GWNET-G2 CAF application...");
+  if (app_event_manager_init()) {
+    LOG_ERR("Application Event Manager not initialized");
+  } else {
+    module_set_state(MODULE_STATE_READY);
+  }
 }
-
